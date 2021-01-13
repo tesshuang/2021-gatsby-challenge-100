38022504c9329b2a5e21f260eb4d97d694d57d8b
Tess Huang <tesshuangxj@yahoo.com>
2021-01-12 17:14:46 -0800

    Use project environment and update gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: "AudioC0RE",
@@ -7,7 +11,7 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
         spaceId: "xqo18pqmyhce",
       },
     },
