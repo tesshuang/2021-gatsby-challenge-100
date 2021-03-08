56af1bfe311d74a5f03b30b43f34d11180e16e28
Tess Huang <tesshuangxj@yahoo.com>
2021-03-08 14:52:46 -0800

    Add formium to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,32 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 module.exports = {
   siteMetadata: {
-    title: "AudioC0RE",
+    title: 'AudioC0RE',
     siteUrl: `https://www.example.com`,
     description: `AudioC0RE is the leading headphones sharing app in the world.`,
   },
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        spaceId: "xqo18pqmyhce",
+        spaceId: 'xqo18pqmyhce',
       },
     },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -35,11 +35,18 @@ module.exports = {
         short_name: `AudioC0RE`,
         start_url: `/`,
         background_color: `#663399`,
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
         theme_color: `#663399`,
         display: `minimal-ui`,
       },
     },
-    "@chakra-ui/gatsby-plugin"
+    '@chakra-ui/gatsby-plugin',
+    {
+      resolve: `gatsby-source-formium`,
+      options: {
+        projectId: process.env.GATSBY_FORMIUM_PROJECTID,
+        accessToken: process.env.FORMIUM_TOKEN,
+      },
+    },
   ],
 };
