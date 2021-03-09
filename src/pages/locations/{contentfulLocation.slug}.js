da5906ebc5157da6123c0f5b2f75d61aedc0b07a
Tess Huang <tesshuangxj@yahoo.com>
2021-03-09 15:13:09 -0800

    Update query and display content from CMS

--- src/pages/locations/{contentfulLocation.slug}.js
+++ src/pages/locations/{contentfulLocation.slug}.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import { graphql, Link } from 'gatsby';
+import ReactMarkdown from 'react-markdown';
 
 const CityPage = ({ data }) => {
   console.log(data);
+  const { contentfulLocation } = data;
   return (
     <main>
-      <h1>City</h1>
+      <header>
+        <Link to="/home">Go back to Home</Link>
+      </header>
+      <h1>{contentfulLocation.title}</h1>
+      <ReactMarkdown source={contentfulLocation.description.description} />
     </main>
   );
 };
@@ -12,8 +19,8 @@ const CityPage = ({ data }) => {
 export default CityPage;
 
 export const query = graphql`
-  query {
-    contentfulLocation {
+  query($slug: String!) {
+    contentfulLocation(slug: { eq: $slug }) {
       title
       description {
         description
