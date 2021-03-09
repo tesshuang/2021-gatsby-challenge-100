fd8230779cff148a34d431c37a6d74fecbfc7347
Tess Huang <tesshuangxj@yahoo.com>
2021-03-09 14:55:47 -0800

    Update index file of locations

--- src/pages/locations/index.js
+++ src/pages/locations/index.js
@@ -1,11 +1,33 @@
 import * as React from 'react';
+import { graphql, Link } from 'gatsby';
 
-const AllLocationPage = () => {
+const AllLocationPage = ({ data }) => {
+  const { allContentfulLocation } = data;
   return (
     <main>
-      <h1>All locations</h1>
+      <h1>Our Office locations</h1>
+      <ul>
+        {allContentfulLocation.edges.map((location, index) => (
+          <Link to={location.node.slug} key={index}>
+            <li>{location.node.title}</li>
+          </Link>
+        ))}
+      </ul>
     </main>
   );
 };
 
 export default AllLocationPage;
+
+export const query = graphql`
+  query {
+    allContentfulLocation {
+      edges {
+        node {
+          title
+          slug
+        }
+      }
+    }
+  }
+`;
