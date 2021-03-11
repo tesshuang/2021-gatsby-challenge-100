d77ac07db899b23e2389fbb01ba47a2d488c21a8
Tess Huang <tesshuangxj@yahoo.com>
2021-03-10 21:45:30 -0800

    Add seo value from to blog post

--- src/pages/blogs/{wpPost.slug}.js
+++ src/pages/blogs/{wpPost.slug}.js
@@ -1,27 +1,54 @@
 import React from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
 import SEO from '../../components/SEO';
-import { Heading } from '@chakra-ui/react';
+import {
+  Heading,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Stack,
+} from '@chakra-ui/react';
 
 export default function BlogPage({ data }) {
   const { wpPost } = data;
   console.log(wpPost);
   return (
     <>
-      <SEO title="home" />
-      <Heading as="h1" size="2xl">
-        {wpPost.title}
-      </Heading>
-      <div dangerouslySetInnerHTML={{ __html: wpPost.content }} />
+      {/* TODO: some seo value is null */}
+      <SEO
+        title={wpPost.seo?.title | wpPost.title}
+        description={wpPost.seo?.metaDesc | ''}
+      />
+      <Stack pacing={6}>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/home">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/blogs">Blogs</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem isCurrentPage>
+            <BreadcrumbLink href="#">{wpPost.title}</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <Heading as="h1" size="2xl">
+          {wpPost.title}
+        </Heading>
+        <div dangerouslySetInnerHTML={{ __html: wpPost.content }} />
+      </Stack>
     </>
   );
 }
 
-export const pageQuery = graphql`
+export const query = graphql`
   query($slug: String!) {
     wpPost(slug: { eq: $slug }) {
       title
       content
+      seo {
+        title
+        metaDesc
+      }
     }
   }
 `;
